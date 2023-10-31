7d918049c9247942f9a4e5ab538d262409e9ac1a
QuocHuyyy0 <tranquochuyatk@gmail.com>
2023-10-31 22:54:35 +0700

    “Add”

---
+++ HTML/login.js
@@ -0,0 +1,7 @@
+function signup(){
+    var taikhoan = document.getElementById("tranquochuy23").value
+    var matkhau = document.getElementById("huy2003").value
+    if (taikhoan==="tranquochuy23"&&matkhau==="huy2003"){
+        window.open("index.html")
+    }
+}
